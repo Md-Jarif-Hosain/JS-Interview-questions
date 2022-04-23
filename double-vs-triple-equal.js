98242a9f51be3f6e5dbb6a7761263dc9b0c2ce9c
Jarif Hosain <hosainjarif@gmail.com>
2022-04-24 01:14:15 +0600

    undefined vs nulldone

--- double-vs-triple-equal.js
+++ double-vs-triple-equal.js
@@ -1,5 +1,46 @@
-const first = 2;
-const second = '2'
+let first = 2;
+let second = '2'
+
+if (first == second)// compare only values
+{
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+};
+
+
+
+if (first === second)// compare values and data types also.
+{
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+};
+
+
+
+
+
+first = 0;
+second = false; // In JS, 0 = false value.
+
+if (first == second)// compare only values
+{
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+};
+
+
+
+if (first === second)// compare values and data types also.
+{
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+};
+first = 1;
+second = true;// In JS, 1 = true value.
 
 if (first == second)// compare only values
 {
