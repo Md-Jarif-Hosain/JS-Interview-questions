83de07da259ae297abad045c91481e90b6eef0b3
Jarif Hosain <hosainjarif@gmail.com>
2022-04-24 00:34:11 +0600

    true false file added

---
+++ true-false.js
@@ -0,0 +1,24 @@
+// **************************  Falsy Values :
+// false 
+// 0
+// undefined
+// null 
+// NaN 
+
+// **************************  Truthy Values :
+// string 0 = '0'
+// empty string with wide space 
+// empty array 
+// empty object
+
+
+
+
+
+let name = 0;
+
+if (name || name == 0){
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+};
