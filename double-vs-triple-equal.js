88d5da939534d91f203c7280370041e9346652c4
Jarif Hosain <hosainjarif@gmail.com>
2022-04-24 01:10:22 +0600

    double vs triple equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,18 @@
+const first = 2;
+const second = '2'
+
+if (first == second)// compare only values
+{
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+};
+
+
+
+if (first === second)// compare values and data types also.
+{
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+};
